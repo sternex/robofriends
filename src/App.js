16d0b62d38bb8f8365bfd332c66d3266dfa0ce2f
Joshua Nieves <josh_nieves@hotmail.com>
2021-11-18 23:43:13 -0400

    Add font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "./CardList";
 import { robots } from "./robotList";
 import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends React.Component {
     constructor() {
